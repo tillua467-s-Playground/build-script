e271699b015c7c57d8ecfbf4e33921b56015daf6
Tillua467 <128046674+tillua467@users.noreply.github.com>
2026-03-15 19:16:38 +0600

    Clone sm6150-common branch 'test'

--- clone.sh
+++ clone.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/tillua467-s-Playground/android_device_xiaomi_phoenix device/xiaomi/phoenix
 
-git clone https://github.com/tillua467-s-Playground/android_device_xiaomi_sm6150-common device/xiaomi/sm6150-common
+git clone https://github.com/tillua467-s-Playground/android_device_xiaomi_sm6150-common device/xiaomi/sm6150-common -b test 
 
 git clone https://github.com/tillua467-s-Playground/android_kernel_xiaomi_phoenix kernel/xiaomi/sm6150
 
