476975f73f99fb392399534352b597895cc50278
Tillua467 <128046674+tillua467@users.noreply.github.com>
2026-04-04 17:53:41 +0600

    drop DolbyUI and track slmk for kernel and main for common device tree

--- clone.sh
+++ clone.sh
@@ -2,16 +2,14 @@
 
 git clone https://github.com/tillua467-s-Playground/android_device_xiaomi_phoenix device/xiaomi/phoenix
 
-git clone https://github.com/tillua467-s-Playground/android_device_xiaomi_sm6150-common device/xiaomi/sm6150-common -b test 
+git clone https://github.com/tillua467-s-Playground/android_device_xiaomi_sm6150-common device/xiaomi/sm6150-common
 
-git clone https://github.com/tillua467-s-Playground/android_kernel_xiaomi_phoenix kernel/xiaomi/sm6150
+git clone https://github.com/tillua467-s-Playground/android_kernel_xiaomi_phoenix kernel/xiaomi/sm6150 -b slmk
 
 git clone https://github.com/tillua467-s-Playground/proprietary_vendor_xiaomi_phoenix vendor/xiaomi/phoenix
 
 git clone https://github.com/tillua467-s-Playground/proprietary_vendor_xiaomi_sm6150-common vendor/xiaomi/sm6150-common
 
 git clone https://github.com/tillua467-s-Playground/android_hardware_xiaomi hardware/xiaomi
-
-git clone https://github.com/tillua467-s-Playground/packages_apps_DolbyUI packages/apps/DolbyUI
 
 git clone https://github.com/tillua467-s-Playground/hardware_dolby hardware/dolby
